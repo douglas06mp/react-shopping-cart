db9526e787db673ce53733fc76791a5bd79040dc
douglas <douglas06mp@gmail.com>
2020-04-09 14:57:13 +0800

    add transition group to cart list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Router from './Router';
 import './App.css';
--- src/components/CartList.js
+++ src/components/CartList.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { CartContext } from '../context/cart.context';
 import Cart from './Cart';
 import PriceInfo from './PriceInfo';
@@ -8,19 +9,21 @@ export default function CartList() {
 
   return (
     <div className="container mx-auto mt-4 p-4 flex justify-between">
-      <div className="w-4/5 grid grid-cols-2 gap-8">
+      <TransitionGroup className="w-4/5 grid grid-cols-2 gap-8" appear>
         {carts.length > 0 &&
           carts.map(({ id, name, img, price, amount }) => (
-            <Cart
-              id={id}
-              key={id}
-              name={name}
-              img={img}
-              price={price}
-              amount={amount}
-            />
+            <CSSTransition key={id} classNames="slide" timeout={300}>
+              <Cart
+                id={id}
+                key={id}
+                name={name}
+                img={img}
+                price={price}
+                amount={amount}
+              />
+            </CSSTransition>
           ))}
-      </div>
+      </TransitionGroup>
       <PriceInfo />
     </div>
   );
