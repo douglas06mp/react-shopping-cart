2a0f43415d7cc4836ea07360fc5f65ddabf155e4
douglas <douglas06mp@gmail.com>
2020-04-09 15:17:47 +0800

    Updates

---
+++ precache-manifest.e7feafbc807b7d27d6f96d49e6d1177e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23d15b23ae410f06a1114ea8fe00d0d",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "3726d88ca99389176dff",
+    "url": "/react-shopping-cart/static/css/main.7fac7b74.chunk.css"
+  },
+  {
+    "revision": "6eb92caf541147b60a2f",
+    "url": "/react-shopping-cart/static/js/2.500cb25b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-shopping-cart/static/js/2.500cb25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3726d88ca99389176dff",
+    "url": "/react-shopping-cart/static/js/main.a4e620e6.chunk.js"
+  },
+  {
+    "revision": "69ee8159f3df2c18ac30",
+    "url": "/react-shopping-cart/static/js/runtime-main.603380dd.js"
+  },
+  {
+    "revision": "7e1029517045314fc591d715e3500851",
+    "url": "/react-shopping-cart/static/media/beer.7e102951.jpg"
+  },
+  {
+    "revision": "f1c00a034e9d82052c024854fa90e860",
+    "url": "/react-shopping-cart/static/media/pie.f1c00a03.jpg"
+  },
+  {
+    "revision": "73404b51da6637f863634ad5b8f9df19",
+    "url": "/react-shopping-cart/static/media/sandwich.73404b51.jpg"
+  },
+  {
+    "revision": "9a5527010faa1e1aad498ad7833534b2",
+    "url": "/react-shopping-cart/static/media/steak.9a552701.jpg"
+  },
+  {
+    "revision": "2b6e1cd968e810652157c935b1202ff8",
+    "url": "/react-shopping-cart/static/media/water.2b6e1cd9.jpg"
+  }
+]);
