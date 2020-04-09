6fdfee9bbc61b1bc9d6fe7bdf8be27810ba26d6b
douglas <douglas06mp@gmail.com>
2020-04-09 14:08:16 +0800

    add price in PriceInfo

--- src/components/PriceInfo.js
+++ src/components/PriceInfo.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../context/cart.context';
 
 export default function PriceInfo() {
+  const carts = useContext(CartContext);
+
+  const price = carts.reduce((acc, next) => {
+    const { price, amount } = next;
+    return price * amount + acc;
+  }, 0);
+
   return (
     <div className="w-1/6 h-64 bg-orange-200 rounded shadow-lg text-center py-10">
       <i className="fas fa-shopping-cart text-6xl text-orange-500"></i>
       <p className="font-bold mt-10">Tatal Cost:</p>
-      <p className="font-bold text-2xl">$2500</p>
+      <p className="font-bold text-2xl">${price}</p>
     </div>
   );
 }
